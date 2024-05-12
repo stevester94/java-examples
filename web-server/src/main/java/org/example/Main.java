fe2e41372afdd5ec59507fb942938123d0377253
Steven Mackey <stevester94@gmail.com>
2024-05-12 11:06:19 -0700

    Begin intellij web server

---
+++ web-server/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int[]ar = {1,2,3};
+
+        System.out.print("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+            System.out.println( x );
+        }
+    }
+}
